3498c1857eaa48f7c83eaa8d52bada041b63a335
aboud20-meet <aboud20@meet.mit.edu>
2019-01-13 02:48:55 +0200

    part2

--- app.py
+++ app.py
@@ -3,20 +3,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return render_template("index.html")
-@app.route('/products')
-def products():
-    return render_template("products.html")
+    return render_template("home_page.html")
+@app.route('/female')
+def female():
+    return render_template("female.html")
+
 if __name__ == '__main__':
    app.run(debug = True)
 
-@app.route('/')
-def home_page():
-    return render_template("products.html")
+
 
 @app.route('/')
 def home_page():
-    return render_template("ok.html")
+    return render_template("female.html")
+
+@app.route('/')
+def home_page():
+    return render_template("home_page.html")
     
 
    	
