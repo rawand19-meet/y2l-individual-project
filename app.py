c797b8197b387b9e60b03823f83370c7e3a244bb
aboud20-meet <aboud20@meet.mit.edu>
2019-01-15 21:33:35 +0200

    part3

--- app.py
+++ app.py
@@ -8,18 +8,28 @@ def home_page():
 def female():
     return render_template("female.html")
 
+@app.route('/male')
+def male():
+    return render_template("male.html")
+
+@app.route('/backgroundcolor')
+def backgroundcolor():
+    return render_template("home_page.css")
+
+
+
 if __name__ == '__main__':
    app.run(debug = True)
 
 
 
-@app.route('/')
-def home_page():
-    return render_template("female.html")
+# @app.route('/')
+# def home_page():
+#     return render_template("female.html")
 
-@app.route('/')
-def home_page():
-    return render_template("home_page.html")
+# @app.route('/')
+# def home_page():
+#     return render_template("home_page.html")
     
 
    	
