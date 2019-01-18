c3ec7ed210a140ff33a5e0017c14dd65c3ed442a
aboud20-meet <aboud20@meet.mit.edu>
2019-01-18 03:29:50 +0200

    yes2

--- app.py
+++ app.py
@@ -4,10 +4,15 @@ app = Flask(__name__)
 @app.route('/')
 def home_page():
     return render_template("home_page.html")
-    
+
 @app.route('/female')
 def female():
     return render_template("female.html")
+
+@app.route('/female2')
+def female2():
+    return render_template("female2.html")
+
 
 @app.route('/male')
 def male():
