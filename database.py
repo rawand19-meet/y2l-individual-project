f182910e53652a644ced7174502c21a942c76a41
aboud20-meet <aboud20@meet.mit.edu>
2019-01-27 23:15:59 +0200

    wfgg

--- database.py
+++ database.py
@@ -85,3 +85,37 @@ def query_by_category(category):
 
 
 print(query_all())
+,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+om model import Base, User     
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///project.db')
+Base.metadata.create_all(engine)
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+def add_user(name,username, password):
+    print("Added a user!")
+    user = User(name=name,username=username ,password=password)
+    session.add(user)
+    session.commit()
+
+def get_all_users():
+  users = session.query(User).all()
+  return users
+
+
+def query_by_name(name):
+  user= session.query(User).filter_by(name=name).first()
+  return user 
+
+def query_by_username(username):
+  user= session.query(User).filter_by(username=username).first()
+  print 'user: ' + str(user)
+  return user 
+
+def query_by_password(password):
+  users= session.query(User).filter_by(password=password).all()
+  return users
+  ,,,,,,,,,,,,,,,,,,,,,,,,,,,,
--- model.py
+++ model.py
@@ -14,4 +14,28 @@ class Products(Base):
   name = Column(String)
   picture = Column(String)
   category =Column(String)
+,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
 
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker 
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+
+
+class User(Base):
+  __tablename__="user"
+  id=Column(Integer, primary_key=True)
+  name=Column(String)
+  username=Column(String, unique=True )
+  password=Column(String)
+ 
+  
+
+
+  def __repr__(self):
+    return ("user name:{}, user password:{}".format(self.name, self.password))
+    ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
